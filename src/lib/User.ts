4d364e48d8d2673c7cf81687c4a3824982ebc786
Copystrike <verbraeckennick@gmail.com>
2021-02-26 04:50:18 +0100

    added getComments!

--- src/lib/User.ts
+++ src/lib/User.ts
@@ -5,17 +5,10 @@ async function getUser(userID: number, clientID: string, app_version: string, ap
     return await requestSoundCloud(`users/${userID}?client_id=${clientID}&app_version=${app_version}&app_locale=${app_locale}`)
 }
 
-async function getComments(userID: number, clientID: string, threaded: number, filterReplies: number, offset: number, linked_partitioning: number = 0,
-                           app_version: string, app_locale: string): Promise<SoundCloudUserProfile> {
-    return await requestSoundCloud(`users/${userID}/comments?
-    client_id=${clientID}
-    &threaded=${threaded}
-    &filter_replies=${filterReplies}
-    &offset=${offset}
-    &linked_partitioning=${linked_partitioning}
-    &app_version=${app_version}
-    &app_locale=${app_locale}
-    `)
+// todo change any to actual response
+async function getComments(userID: number, clientID: string, limit: number, offset: number, linked_partitioning: number, filterReplies: number,
+                           app_version: string, app_locale: string): Promise<any> {
+    return await requestSoundCloud(`users/${userID}/comments?client_id=${clientID}&limit=${limit}&offset=${offset}&linked_partitioning=${linked_partitioning}&app_version=${app_version}&app_locale=${app_locale}`)
 }
 
 export {
--- src/request/RequestManager.ts
+++ src/request/RequestManager.ts
@@ -3,13 +3,12 @@ import fetch from "node-fetch";
 const soundcloudAPIV2 = "https://api-v2.soundcloud.com/";
 
 async function requestSoundCloud(path: String): Promise<any> {
-
     try {
-        const response = await fetch(soundcloudAPIV2 + path)
+        return await fetch(soundcloudAPIV2 + path)
             .then(website => website.text())
             .then(data => data);
     } catch (error) {
-        console.log()
+        console.log(error);
     }
 
 }
